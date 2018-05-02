f0d311fc64d2becc54492d3cb7ae513ba59c700a
Ryosuke.Watanabe <ryosuke.watanabe@mizuho-ir.co.jp>
2018-05-02 17:14:54 +0900

    add

--- routes/register.js
+++ routes/register.js
@@ -1,31 +1,9 @@
 // import package
 var invoke = require('./util/invoke.js')
 var express = require('express');
-var Fabric_Client = require('fabric-client')
 var path = require('path');
 var os = require('os')
-var peer = require('./util/peer')
-var orderer = require('./util/orderer')
-var Fabric_Client = require('fabric-client')
 var router = express.Router();
-
-// create new fabric client
-var fabric_client = new Fabric_Client()
-fabric_client.loadFromConfig(path.join(__dirname, './artifacts/network.yaml'))
-
-// setup the fabric network
-var channel = fabric_client.newChannel('identity')
-
-// add peer and orderer to the channel
-channel.addOrderer(orderer)
-channel.addPeer(peer.org1.peer0.peerObject)
-channel.addPeer(peer.org1.peer1.peerObject)
-channel.addPeer(peer.org2.peer0.peerObject)
-channel.addPeer(peer.org2.peer1.peerObject)
-
-var member_user = null
-var store_path = path.join(__dirname, './hfc-key-store')
-var tx_id = null
 
 /* register user Infomation */
 router.post('/', function (req, res, next) {
