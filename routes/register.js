93350b8761c75b6338b9c9ccf759df6be66ecd8b
Ryosuke <soccermfw_9@yahoo.co.jp>
2018-05-06 15:39:23 +0900

    add

--- routes/register.js
+++ routes/register.js
@@ -3,10 +3,22 @@ var invoke = require('./util/invoke.js')
 var express = require('express');
 var path = require('path');
 var os = require('os')
+var mysql = require('mysql')
 var router = express.Router();
+
+// connect to mysql
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'s7_fsx..',
+    database:'Identity'
+})
+
+
 
 /* register user Infomation */
 router.post('/', function (req, res, next) {
+    var serviceList=[]
     var request = {
         targets: '',
         chaincodeId: 'operateUserInfo',
@@ -17,13 +29,30 @@ router.post('/', function (req, res, next) {
     }
 
     invoke.invokeFunction('user1', request)
-  
-    //if invoke has finished, create login session
-    req.session.userID=req.body.userID
-    console.log(req.session.userID)
-    res.render('register', {
+
+    // we have to change query and table difinition
+    var serviceQuery = 'SELECT name from SERVICE_LIST WHERE id IN (SELECT id from SERVICE_USES_LIST WHERE email=true and accountname=false and firstname=false and lastname=false and phone=false and postalcode=false and address=false);'
+    connection.connect()
+    connection.query(serviceQuery,function(error,rows,fields){
+        if(error){
+            console.log(error)
+        }else{
+            for(i=0; i<rows.length; i++){
+                serviceList.push(rows[i].name)
+                console.log(rows[i].name)
+            }
+        }
+
+        console.log(serviceList)
+        connection.end()
+
+        //if invoke has finished, create login session
+        req.session.userID=req.body.userID
+        res.render('register', {
         title: 'register finished',
-        userID:req.session.userID
+        userID:req.session.userID,
+        serviceList:serviceList
+        })
     })
 });
 
