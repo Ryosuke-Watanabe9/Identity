49cc0add746fd042bd833e1df7748307b3a01042
Ryosuke <soccermfw_9@yahoo.co.jp>
2018-05-06 18:50:28 +0900

    add

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 // import package
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
+var express = require('express')
+var path = require('path')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var session = require('express-session')
 
 //create user
 //require('./routes/enrollAdmin.js')
 //require('./routes/registerUser.js')
 
 // routing
-var index = require('./routes/index');
-var register = require('./routes/register');
+var index = require('./routes/index')
+var register = require('./routes/register')
+var addUserInfo = require('./routes/addUserInfo')
 
-var app = express();
+var app = express()
 app.use(session({
     secret: 'my-special-secret',
     resave: false,
@@ -24,29 +25,30 @@ app.use(session({
         maxAge:1000*60*60
     }
 }))
-app.use(express.static("public"));
-app.use(logger('dev'));
+app.use(express.static("public"))
+app.use(logger('dev'))
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 // Body ParserでURLエンコーディングとJSONエンコーディングをONにするもの
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // route
-app.use('/', index);
-app.use('/register', register);
+app.use('/', index)
+app.use('/register', register)
+app.use('/addUserInfo', addUserInfo)
 
 
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    var err = new Error('Not Found')
+    err.status = 404
+    next(err)
 });
 
 /*
@@ -62,7 +64,7 @@ app.use(function (err, req, res, next) {
 });
 */
 var server = app.listen(3200, function(){
-    console.log("Node.js is listening to PORT:" + server.address().port);
-});
+    console.log("Node.js is listening to PORT:" + server.address().port)
+})
 
-module.exports = app;
+module.exports = app
